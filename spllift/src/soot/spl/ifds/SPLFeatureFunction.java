e4b3019fcc968414f112b74b0e4225f646e61463
Miguel Velez <miguelvelez@mijecu25.com>
2017-07-31 09:08:03 -0400

    Added logging

--- spllift/src/soot/spl/ifds/SPLFeatureFunction.java
+++ spllift/src/soot/spl/ifds/SPLFeatureFunction.java
@@ -25,20 +25,35 @@ public class SPLFeatureFunction implements EdgeFunction<IConstraint> {
 	}
 
 	public EdgeFunction<IConstraint> composeWith(EdgeFunction<IConstraint> secondFunction) {
-		if(secondFunction instanceof EdgeIdentity || secondFunction instanceof AllTop) return this;
+		if(secondFunction instanceof EdgeIdentity || secondFunction instanceof AllTop) {
+			System.out.println("COMPOSING >>>>> [" + features + "] OR [" + secondFunction + "] ========> " + this);
+			return this;
+		}
 		
 		SPLFeatureFunction other = (SPLFeatureFunction)secondFunction;
+		SPLFeatureFunction res = new SPLFeatureFunction(features.and(other.features));
+		System.out.println("COMPOSING >>>>> [" + features + "] OR [" + secondFunction + "] ========> " + res);
 		
 		return new SPLFeatureFunction(features.and(other.features));
 	}
 
 	public EdgeFunction<IConstraint> joinWith(EdgeFunction<IConstraint> otherFunction) {
 		//here we implement union/"or" semantics
-		if(otherFunction instanceof AllTop) return this;
-		if(otherFunction instanceof EdgeIdentity) return otherFunction;
+		if(otherFunction instanceof AllTop) {
+			System.out.println("JOINING >>>>> [" + features + "] OR [" + otherFunction + "] ========> " + this);
+			return this;
+		}
+		
+		if(otherFunction instanceof EdgeIdentity) {
+			System.out.println("JOINING >>>>> [" + features + "] OR [" + otherFunction + "] ========> " + otherFunction);
+			return otherFunction;
+		}
 
 		SPLFeatureFunction other = (SPLFeatureFunction)otherFunction;
-		return new SPLFeatureFunction(features.or(other.features));
+		SPLFeatureFunction res = new SPLFeatureFunction(features.or(other.features));
+		System.out.println("JOINING >>>>> [" + features + "] OR [" + otherFunction + "] ========> " + res);
+		
+		return res;
 	}
 	
 	public boolean equalTo(EdgeFunction<IConstraint> other) {
